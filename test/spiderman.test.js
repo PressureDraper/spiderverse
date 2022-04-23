ef16bda896f3377aef87ed33f2ca97a19257aac0
PressureDraper <warmarines00@hotmail.com>
2022-04-23 18:14:44 -0500

    add: making fail spiderman class test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+describe("Unit Test for Spiderman Class", () => {
+    test('1) Creating spiderman object', () => {
+        //Escribimos aqui el código que queremos usar tal cual
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        //Validamos que el código funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
